d39108a919e140b960fe9819207afba19dcc0d4d
rvmelo <robertomelo7@hotmail.com>
2024-03-05 09:25:33 -0300

    feat: add header profile

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,5 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true, // Enable cookies with jwt located on the frontend to be sent to backend
 })
