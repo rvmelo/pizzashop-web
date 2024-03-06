9fd6d84bfffe434cf7d2d0a8918ac35bcbf22a54
rvmelo <robertomelo7@hotmail.com>
2024-03-06 12:58:01 -0300

    feat: add order listing

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,6 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { Variable } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
