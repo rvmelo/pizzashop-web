785f8cf038bba4dff3c6bf78abf053b93f23aa81
rvmelo <robertomelo7@hotmail.com>
2024-03-08 00:39:39 -0300

    feat: add order details

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,33 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrderDetailsParams {
+  orderId: string
+}
+
+interface GetOrderDetailsResponse {
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  id: string
+  createdAt: string
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: {
+    id: string
+    priceInCents: number
+    quantity: number
+    product: {
+      name: string
+    }
+  }[]
+}
+
+export async function getOrderDetails({
+  orderId,
+}: GetOrderDetailsParams): Promise<GetOrderDetailsResponse> {
+  const response = await api.get<GetOrderDetailsResponse>(`/orders/${orderId}`)
+
+  return response.data
+}
