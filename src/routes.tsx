be8ede5acd791b50c3e6c5fa5068c4d2a859ce04
rvmelo <robertomelo7@hotmail.com>
2024-03-12 17:45:16 -0300

    feat: add request error and error boundary

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -32,4 +33,5 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  { path: '*', element: <NotFound /> },
 ])
