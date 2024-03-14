38cbaced8f7b0a5d207fb59b73c9f879eb4af313
rvmelo <robertomelo7@hotmail.com>
2024-03-14 20:41:24 -0300

    feat: add service mock worker

--- src/pages/auth/sign-in.spec.tsx
+++ src/pages/auth/sign-in.spec.tsx
@@ -1,4 +1,4 @@
-import { QueryClientProvider, useQueryClient } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { render } from '@testing-library/react'
 import React from 'react'
 import { HelmetProvider } from 'react-helmet-async'
